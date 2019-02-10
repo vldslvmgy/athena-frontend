ddc2c031f5bcdcf8f3944ae4c10c122c2cf720f5
Vlad Magay <wiko.kz@yandex.kz>
2019-02-10 15:36:57 -0800

    initialized profile and introduced router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Profile from './components/Profile/Profile';
+import { Route } from 'react-router';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <Route path="/profile" component={Profile} />
+      </Router>
     );
   }
 }
