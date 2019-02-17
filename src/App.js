92170090ff57c3870d7297699572212d79c55bd5
Vlad Magay <wiko.kz@yandex.kz>
2019-02-17 13:49:08 -0800

    Jordan knows a lot

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import Profile from './components/Profile/Profile';
 import { Route } from 'react-router';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
-
-
+import Navbar from './components/Navbar/Navbar'
 class App extends Component {
   render() {
     return (
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import {
+    MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavItem, MDBNavLink, MDBNavbarToggler, MDBCollapse, MDBFormInline,
+    MDBDropdown, MDBDropdownToggle, MDBDropdownMenu, MDBDropdownItem
+} from "mdbreact";
+
+class Navbar extends React.Component {
+    render() {
+        return (
+            <MDBNavbar color="indigo" dark expand="md">
+                <MDBNavbarBrand>
+                    <strong className="white-text">Navbar</strong>
+                </MDBNavbarBrand>
+                <MDBNavbarToggler onClick={this.toggleCollapse} />
+                <MDBCollapse id="navbarCollapse3">
+                    <MDBNavbarNav left>
+                        <MDBNavItem active>
+                            <MDBNavLink to="#!">Home</MDBNavLink>
+                        </MDBNavItem>
+                        <MDBNavItem>
+                            <MDBNavLink to="#!">Features</MDBNavLink>
+                        </MDBNavItem>
+                        <MDBNavItem>
+                            <MDBNavLink to="#!">Pricing</MDBNavLink>
+                        </MDBNavItem>
+                        <MDBNavItem>
+                            <MDBDropdown>
+                                <MDBDropdownToggle nav caret>
+                                    <span className="mr-2">Dropdown</span>
+                                </MDBDropdownToggle>
+                                <MDBDropdownMenu>
+                                    <MDBDropdownItem href="#!">Action</MDBDropdownItem>
+                                    <MDBDropdownItem href="#!">Another Action</MDBDropdownItem>
+                                    <MDBDropdownItem href="#!">Something else here</MDBDropdownItem>
+                                    <MDBDropdownItem href="#!">Something else here</MDBDropdownItem>
+                                </MDBDropdownMenu>
+                            </MDBDropdown>
+                        </MDBNavItem>
+                    </MDBNavbarNav>
+                    <MDBNavbarNav right>
+                        <MDBNavItem>
+                            <MDBFormInline waves>
+                                <div className="md-form my-0">
+                                    <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" />
+                                </div>
+                            </MDBFormInline>
+                        </MDBNavItem>
+                    </MDBNavbarNav>
+                </MDBCollapse>
+            </MDBNavbar>
+        );
+    }
+}
+
+export default Navbar; 
