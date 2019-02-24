ece7674b6ff7413e2e1649f3c0b1f9cecfe2a484
Vlad Magay <wiko.kz@yandex.kz>
2019-02-24 15:14:32 -0800

    Introduced sidebar, show/hide on click

--- src/components/Lists/Lists.jsx
+++ src/components/Lists/Lists.jsx
@@ -3,22 +3,59 @@ import styled from 'styled-components';
 import { withRouter } from 'react-router-dom';
 
 const Container = styled.div`
-  align-items: left;
-  display: flex;
-  justify-content: space-between;
-  padding: 8px;
-`
+
+  `
 
 class Lists extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      movies:["List1", "List2", "List3"],
+      spotify:["List1"],
+      showMovies:false,
+      showSpotify:false
+    };
+  }
+
+  clickMovies =() => {
+    this.setState({
+      showMovies:!this.state.showMovies
+    })
+  }
+
+  clickSpotify =() => {
+    this.setState({
+      showSpotify:!this.state.showSpotify
+    })
   }
 
   render() {
+    const movieList = this.state.movies.map((movie) => <li key={movie}>{movie}</li>);
+    const spotifyList = this.state.spotify.map((spotify) => <li key={spotify}>{spotify}</li>);
     return (
       <Container>
-        <p>lalala</p>
+        <ul>
+          <li>
+            <p onClick={this.clickMovies}>Movies</p>
+            {this.state.showMovies ? 
+              <ul>
+                {movieList}
+              </ul>
+              :
+              null
+            }
+          </li>
+          <li>
+            <p onClick={this.clickSpotify}>Spotify</p>
+            {this.state.showSpotify ? 
+              <ul>
+                {spotifyList}
+              </ul>
+              :
+              null
+            }
+          </li>
+        </ul>
       </Container>
     )
   }
